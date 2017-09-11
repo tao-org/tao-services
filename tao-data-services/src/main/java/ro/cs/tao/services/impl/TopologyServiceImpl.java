b60bff6703e263c0ae35b2ba0308d7707b64808c
Cosmin Cara <kraftek@c-s.ro>
2017-09-11 15:54:05 +0300

    Delegation of methods for topology

--- tao-data-services/src/main/java/ro/cs/tao/services/impl/TopologyServiceImpl.java
+++ tao-data-services/src/main/java/ro/cs/tao/services/impl/TopologyServiceImpl.java
@@ -3,8 +3,8 @@ package ro.cs.tao.services.impl;
 import org.springframework.stereotype.Service;
 import ro.cs.tao.services.interfaces.TopologyService;
 import ro.cs.tao.topology.NodeDescription;
+import ro.cs.tao.topology.TopologyManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,49 +15,21 @@ public class TopologyServiceImpl implements TopologyService {
 
     @Override
     public NodeDescription findById(String hostName) {
-        NodeDescription node = new NodeDescription();
-        node.setHostName("host_sample");
-        node.setIpAddr("10.0.0.1");
-        node.setUserName("user");
-        node.setUserPass("drowssap");
-        node.setProcessorCount(4);
-        node.setMemorySizeGB(16);
-        node.setDiskSpaceSizeGB(500);
-        return node;
+       return TopologyManager.getInstance().get(hostName);
     }
 
     @Override
     public List<NodeDescription> list() {
-        List<NodeDescription> list = new ArrayList<NodeDescription>();
-        NodeDescription node = new NodeDescription();
-        node.setHostName("host_sample_1");
-        node.setIpAddr("10.0.0.1");
-        node.setUserName("user");
-        node.setUserPass("drowssap");
-        node.setProcessorCount(4);
-        node.setMemorySizeGB(16);
-        node.setDiskSpaceSizeGB(500);
-        list.add(node);
-
-        node = new NodeDescription();
-        node.setHostName("host_sample_2");
-        node.setIpAddr("10.0.0.2");
-        node.setUserName("user");
-        node.setUserPass("drowssap");
-        node.setProcessorCount(4);
-        node.setMemorySizeGB(16);
-        node.setDiskSpaceSizeGB(500);
-        list.add(node);
-        return list;
+       return TopologyManager.getInstance().list();
     }
 
     @Override
     public void save(NodeDescription node) {
-        //no-op
+        TopologyManager.getInstance().update(node);
     }
 
     @Override
     public void delete(String name) {
-        //no-op
+        TopologyManager.getInstance().remove(name);
     }
 }
