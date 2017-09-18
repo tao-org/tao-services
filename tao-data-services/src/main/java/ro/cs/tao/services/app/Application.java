582e48cdf5ba14ad0f05e51993110e9d24e66587
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2017-09-18 10:13:45 +0300

    Updates for persistence and spring bridge.

--- tao-data-services/src/main/java/ro/cs/tao/services/app/Application.java
+++ tao-data-services/src/main/java/ro/cs/tao/services/app/Application.java
@@ -2,11 +2,13 @@ package ro.cs.tao.services.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author Cosmin Cara
  */
-@SpringBootApplication(scanBasePackages = { "ro.cs.tao.services" })
+@SpringBootApplication(scanBasePackages = { "ro.cs.tao.services", "ro.cs.tao.persistence" })
+@ImportResource("classpath:tao-persistence-context.xml")
 public class Application {
 
     public static void main(String[] args) {
