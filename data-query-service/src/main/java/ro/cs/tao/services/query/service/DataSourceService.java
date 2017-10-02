9ea6c80f0a61e46c857143b868f6d5b07aa2726d
Cosmin Cara <kraftek@c-s.ro>
2017-10-02 19:12:51 +0300

    Added method to retrieve all data sources with parameters. All services extend Identifiable, so they can expose friendly names/ids

--- data-query-service/src/main/java/ro/cs/tao/services/query/service/DataSourceService.java
+++ data-query-service/src/main/java/ro/cs/tao/services/query/service/DataSourceService.java
@@ -2,19 +2,23 @@ package ro.cs.tao.services.query.service;
 
 import ro.cs.tao.datasource.param.ParameterDescriptor;
 import ro.cs.tao.eodata.EOProduct;
+import ro.cs.tao.services.query.model.DataSourceInstance;
 import ro.cs.tao.services.query.model.Query;
 
 import java.text.ParseException;
 import java.util.List;
+import java.util.SortedSet;
 
 /**
  * @author Cosmin Cara
  */
 public interface DataSourceService {
 
-    List<String> getSupportedSensors();
+    SortedSet<String> getSupportedSensors();
 
     List<String> getDatasourcesForSensor(String sensorName);
+
+    List<DataSourceInstance> getDatasourceInstances();
 
     List<ParameterDescriptor> getSupportedParameters(String sensorName, String dataSourceClassName);
 
