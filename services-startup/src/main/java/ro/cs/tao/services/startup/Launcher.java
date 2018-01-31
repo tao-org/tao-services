d0e954a322dd4207fe8161f9113822f018f401a6
Cosmin Cara <kraftek@c-s.ro>
2018-01-31 18:25:10 +0200

    A lot of refactorings

--- services-startup/src/main/java/ro/cs/tao/services/startup/Launcher.java
+++ services-startup/src/main/java/ro/cs/tao/services/startup/Launcher.java
@@ -1,14 +1,20 @@
 package ro.cs.tao.services.startup;
 
+import org.springframework.boot.ApplicationHome;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import ro.cs.tao.configuration.ConfigurationManager;
 import ro.cs.tao.services.app.ComponentApplication;
 import ro.cs.tao.services.app.ConfigurationApplication;
 import ro.cs.tao.services.app.ContainerApplication;
 import ro.cs.tao.services.app.TopologyApplication;
 import ro.cs.tao.services.monitoring.app.MonitoringApp;
+
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * @author Cosmin Cara
@@ -17,6 +23,22 @@ import ro.cs.tao.services.monitoring.app.MonitoringApp;
 @EnableScheduling
 @ImportResource({"classpath:services-startup-context.xml", "classpath*:tao-persistence-context.xml" })
 public class Launcher {
+    private static final ApplicationHome home;
+
+    static {
+        home = new ApplicationHome(Launcher.class);
+        try {
+            Path configDirectory = homeDirectory().resolve("config");
+            if (!Files.exists(configDirectory)) {
+                Files.createDirectory(configDirectory);
+            }
+            final Field field = ConfigurationManager.class.getField("configFolder");
+            field.setAccessible(true);
+            field.set(null, configDirectory);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         //LifeCycleProcessor.activate();
@@ -26,4 +48,5 @@ public class Launcher {
                 .run(args);
     }
 
+    private static Path homeDirectory() { return home.getDir().getParentFile().toPath(); }
 }
