dc4730f1ea4b57c0cef7690ecd89ef00fcbfde9a
Cosmin Cara <kraftek@c-s.ro>
2018-04-12 14:59:33 +0300

    Externalized tao.properties

---
+++ services-startup/src/main/java/ro/cs/tao/services/ServletConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 CS ROMANIA
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package ro.cs.tao.services;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import ro.cs.tao.configuration.ConfigurationManager;
+
+import java.util.concurrent.Executor;
+import java.util.logging.Logger;
+
+/**
+ * @author Cosmin Cara
+ */
+@Configuration
+public class ServletConfig {
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            String strPort = ConfigurationManager.getInstance().getValue("server.port");
+            int port = strPort != null ? Integer.parseInt(strPort) : 8080;
+            Logger.getLogger(ServletConfig.class.getName()).info("Using server port " + port);
+            container.setPort(port);
+        });
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
