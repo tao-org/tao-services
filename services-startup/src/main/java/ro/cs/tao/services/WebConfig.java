a34763081f13e22439b122bf325b1dd576ed0a3d
kraftek <kraftek@c-s.ro>
2019-10-14 11:42:28 +0300

    Changes from Mon 10/14/2019 11:42:28.34

--- services-startup/src/main/java/ro/cs/tao/services/WebConfig.java
+++ services-startup/src/main/java/ro/cs/tao/services/WebConfig.java
@@ -16,79 +16,35 @@
 
 package ro.cs.tao.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ro.cs.tao.configuration.ConfigurationManager;
+import ro.cs.tao.services.commons.WebConfiguration;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebConfiguration {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String siteBase = ConfigurationManager.getInstance().getValue("site.location", "static");
+    protected String siteURIPath() {
+        final String siteBase = ConfigurationManager.getInstance().getValue("site.location", "static");
         Path sitePath = Paths.get(siteBase);
         if (!sitePath.isAbsolute()) {
             sitePath = TaoServicesStartup.homeDirectory().resolve(siteBase).toAbsolutePath();
         }
-        registry.addResourceHandler("/ui/**").addResourceLocations(sitePath.toUri().toString());
         ConfigurationManager.getInstance().setValue("site.path", sitePath.toString());
-        ConfigurationManager.getInstance().setValue("site.url", sitePath.toUri().toString());
+        final String siteURI = sitePath.toUri().toString();
+        ConfigurationManager.getInstance().setValue("site.url", siteURI);
+        return siteURI;
     }
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/ui/login.html");
-        WebMvcConfigurer.super.addViewControllers(registry);
-    }
+    protected String sitePrefix() { return "/ui"; }
 
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        //Here we add our custom-configured HttpMessageConverter
-        for (int i = 0; i < converters.size(); i++) {
-            if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
-                converters.set(i, jacksonMessageConverter());
-            }
-        }
-        //converters.add(jacksonMessageConverter());
-    }
+    protected String defaultPage() { return "login.html"; }
 
-    /*@Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        //Here we add our custom-configured HttpMessageConverter
-        converters.add(jacksonMessageConverter());
-        converters.add(new ResourceHttpMessageConverter());
-        converters.add(new MappingJackson2XmlHttpMessageConverter());
-    }*/
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorPathExtension(false);
-    }
-
-    private MappingJackson2HttpMessageConverter jacksonMessageConverter(){
-        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper mapper = new ObjectMapper();
-        //Registering Hibernate5Module to support lazy objects
-        Hibernate5Module hibernate5Module = new Hibernate5Module();
-        hibernate5Module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
-        hibernate5Module.enable(Hibernate5Module.Feature.FORCE_LAZY_LOADING);
-        mapper.registerModule(hibernate5Module);
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        messageConverter.setObjectMapper(mapper);
-        return messageConverter;
-    }
 }
